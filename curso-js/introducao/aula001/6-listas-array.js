9b535c4bf456a61a09e888413723ea52da85785b
frykel <98838398+frykel@users.noreply.github.com>
2022-05-31 14:45:15 -0300

    update

--- curso-js/introducao/aula001/6-listas-array.js
+++ curso-js/introducao/aula001/6-listas-array.js
@@ -10,3 +10,9 @@ console.log("Destinos Possíveis:")
 console.log(listaDeDestinos)
 listaDeDestinos.push('Curitiba', 'Natal') //adiciona itens ao vetor (array) após já ter sido declarada
 console.log(listaDeDestinos)
+
+listaDeDestinos.splice(1,1) //esse comando apaga itens do array, no caso (1,1) apaga a partir da segunda posição, um único elemento.
+//posições: [0,1,2,3,4,5...]
+
+console.log(listaDeDestinos)
+console.log(listaDeDestinos[1], listaDeDestinos[0]) //mostra a informação armazenada em uma posição específica.
