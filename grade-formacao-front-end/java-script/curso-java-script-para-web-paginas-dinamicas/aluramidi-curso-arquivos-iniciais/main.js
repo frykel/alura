c571f06f4978c32912312addfa99a8c584c4ea07
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-10-14 14:43:56 -0300

    update 14/10/2022

--- grade-formacao-front-end/java-script/curso-java-script-para-web-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
+++ grade-formacao-front-end/java-script/curso-java-script-para-web-paginas-dinamicas/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,22 @@
+"use strict"
+function TocaSom (seletorAudio){
+    /* Poderiamos deixar nossa estrutura de seleção de audio do jeito que está abaixo:
+    document.querySelector(idElementoAudio).play();,
 
-function TocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    Mas para evitar que alguem passe um atributo que não seja nossas classes de audio, usaremos uma condição como abaixo: */
+
+    const elemento = document.querySelector(seletorAudio)
+    
+    if (elemento && elemento.localName === 'audio'){ // posso substituir elemento != null por elemento que o javascript já ira entender que ele está falando para elementos que existem ou que tem valor e não são nulos.
+
+        /* para termos certeza que o atributo passado é um audio, verificamos se seu nome (localName é de um elemento do tipo audio), conforme acima. */      
+        elemento.play();
+    }
+    else{ //primeiro verificamos se não é um elemento nulo
+
+    alert('Valor inválido');
+
+    }
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -11,7 +27,7 @@ const instrumento = listaDeTeclas[n].classList[1]; //esse comando classlist pesq
 const tecla = listaDeTeclas[n];
 //template string ${}
 const idAudio = `#som_${instrumento}`;
-console.log(idAudio);
+
 tecla.onclick =function (){ 
 /*Neste caso não se pode implementar a função TocaSom diretamente com um atributo ou parâmetro porque o browser irá bloquear (o browser irá exigir interação do usuário). Desta forma implementamos ela sem o ().
 
@@ -22,10 +38,24 @@ TocaSom(idAudio);
 }
 /* Para situações em que o usuário não interaja usando o mouse click, como por exemplo tab, enter ou barra de espaço iremos adicionar o evento onkeydown, que é ativado quando qualquer tecla é pressionada e iremos adicionar uma classe para uso de estilo no css somente quando a interação é dessa maneira*/
 
+/* Para casos de qualquer tecla pressionada ativar a classe usamos o código abaixo:
+
 tecla.onkeydown = function(){
     
     tecla.classList.add('ativa');
 }
+
+Mas se quisermos adicionar a classe em situaçõe específicas (por exemplo somente quando apertarmos a tecla espaço ou enter), faremos como asseguir:
+*/
+
+tecla.onkeydown = function(evento){
+
+     let event = evento.code;
+    if(event === 'Space' || event === 'Enter' || event === 'NumpadEnter'){
+        tecla.classList.add('ativa');
+    }
+}
+
 tecla.onkeyup = function(){
     
     tecla.classList.remove('ativa');
