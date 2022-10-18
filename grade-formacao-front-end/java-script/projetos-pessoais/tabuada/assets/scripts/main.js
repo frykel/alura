55a17233dfe3684af7a78f168b47c2dee0928b07
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-10-18 14:00:01 -0300

    update

--- grade-formacao-front-end/java-script/projetos-pessoais/tabuada/assets/scripts/main.js
+++ grade-formacao-front-end/java-script/projetos-pessoais/tabuada/assets/scripts/main.js
@@ -1,28 +1,22 @@
-let num = document.querySelector('#ntabuada')
-let lista = document.querySelector('#tabuadaid')
-let gerar = document.querySelector('#buttongerar')
-let apagar = document.querySelector('#buttonapagar')
+let num = document.querySelector('#ntabuada');
+let lista = document.querySelector('#tabuadaid');
+let gerar = document.querySelector('#buttongerar');
+let multiplos = document.querySelector('#mult');
 
-function tabuada(numero){
+function remover (){
+    lista.innerHTML= '';
+}
+function tabuada(numero,mults){
+    remover();
     
-    for(var n=0; n<11;n++){
+    for(var n=0; n<=mults.value;n++){
         var res = 0;
-
         res = numero.value * n;
         let item = document.createElement('option');
         item.text= `${numero.value} x ${n} = ${res}`;
         lista.appendChild(item);
-
     }
 }
-function remover (){
-    lista.innerHTML= '';
-}
-
-
 gerar.onclick = function(){
-    tabuada(num);
-}
-apagar.onclick = function(){
-    remover();
+    tabuada(num,multiplos);
 }
