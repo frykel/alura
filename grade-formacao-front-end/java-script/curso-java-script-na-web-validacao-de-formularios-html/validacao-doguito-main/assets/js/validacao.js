24a06e297fd4e02c2a8671733a69d199760d3c5b
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-11-07 15:31:09 -0300

    update

---
+++ grade-formacao-front-end/java-script/curso-java-script-na-web-validacao-de-formularios-html/validacao-doguito-main/assets/js/validacao.js
@@ -0,0 +1,28 @@
+
+const dataNascimento = document.querySelector('#nascimento');
+
+dataNascimento.addEventListener('blur', (evento) =>{ 
+    /*Adicionamos um evento que irá identificar (escutar) quando o usuário da página fizer uma ação, a ação em específico é 'blur', que é quando o campo do input em questão perder o foco. Aplicamos em seguida uma função anônima para implementar a função abaixo: */
+    validaDataNascimento(evento.target);
+})
+
+function validaDataNascimento (input){
+
+    const dataRecebida = new Date(input.value);
+    let mensagem ='';
+
+    if(!maiorQue18(dataRecebida)){ //o ! faz com que maiorQue18 atendendo a condição do if com falsa
+        
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.';
+    
+    }
+    
+    input.setCustomValidity(mensagem);
+}
+
+function maiorQue18 (data){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(),data.getUTCDate())
+    
+    return dataMais18 <= dataAtual
+} 
