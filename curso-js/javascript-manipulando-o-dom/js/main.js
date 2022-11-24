2beb348f490695e42243793bf6bc60d04af417b9
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-11-24 15:08:24 -0300

    update

--- curso-js/javascript-manipulando-o-dom/js/main.js
+++ curso-js/javascript-manipulando-o-dom/js/main.js
@@ -1,4 +1,4 @@
-let imagemRobotron = document.querySelector('#robotron');
+//const imagemRobotron = document.querySelector('#robotron');
 /*Posso delcarar função anônima de duas formas, usando o function (){} ou simplesmente () => {}, conforme as variantes abaixo:
 imagemRobotron.addEventListener("click",function(){
     console.log('Olá, bem vindo')
@@ -12,7 +12,7 @@ imagemRobotron.addEventListener("click", () => {
 
 Diferentemente de funções nomeadas (como a função dizOi() abaixo) as funções anônimas só podem ser chamadas uma vez
 */
-imagemRobotron.addEventListener("click",() =>{
+/*imagemRobotron.addEventListener("click",() =>{
     console.log (f);
 })
 
@@ -20,3 +20,16 @@ function dizOi(){
 console.log('Olá')
 console.log('Bem vindo ao Programa')
 }
+*/
+
+
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+subtrair.addEventListener("click",() => {
+    braco.value -= 1;
+});
+somar.addEventListener("click",() => {
+    braco.value = parseInt(braco.value) + 1;      
+});
