d7d301199541ef21b06ba11ed72137af7c5870ae
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-12-14 09:31:00 -0300

    update

--- projetos-pessoais/projeto-dinamicas-confra-2022/quadrado-certo/assets/js/main.js
+++ projetos-pessoais/projeto-dinamicas-confra-2022/quadrado-certo/assets/js/main.js
@@ -1,5 +1,24 @@
 
 const bloco = document.querySelectorAll('[data-caixa]');
+const botao = document.querySelector('.botao');
+const premio = ["Barra de Chocolate","Trava-língua: O rato roeu a roupa do Rei de Roma (3 vezes)","Dia de Folga","Pote de Nutella","Voucher de R$ 50,00","Ligue para sua mãe e diga que quer uma boneca de aniversário","Barra de Chocolate","Ligue para a última pessoa que mandou mensagem no WhatsApp e diga que está apaixonado(a) por ela","Caixa de BIS"]
+function embaralha(arr){
+    for(var i=0;i<bloco.length;i++){
+        bloco[i].style.fontSize="3rem"
+        bloco[i].style.textAlign="center"
+        bloco[i].innerHTML = i+1
+    }
+    for(var i =arr.length-1 ; i>0 ;i--){
+        var j = Math.floor( Math.random() * (i + 1) ); //random index
+        [arr[i],arr[j]]=[arr[j],arr[i]]; // swap
+    }
+    console.log(arr[0])
+}
+
+botao.addEventListener("click",()=>{
+   embaralha(premio)
+   
+})
 
 bloco.forEach((evento)=>{
 
@@ -14,14 +33,18 @@ function trocaImagem(elemento){
         bloco[i].style.fontSize="2rem"
         bloco[i].style.textAlign="center"
     }
+    for(var i=0;i<bloco.length;i++){
+
+        bloco[elemento-1].innerHTML = premio[elemento-1]
+    }
     
-    if(elemento==="1"){
+   /* if(elemento==="1"){
         
         bloco[0].innerHTML ="Barra de Chocolate"        
     }
     else if(elemento==="2"){
         
-        bloco[1].innerHTML ="Trava-língua: O rato roeu a roupa do Rei de Roma"        
+        bloco[1].innerHTML ="Trava-língua: O rato roeu a roupa do Rei de Roma (3 vezes)"        
     }
     else if(elemento==="3"){
        
@@ -29,7 +52,7 @@ function trocaImagem(elemento){
     }
     else if(elemento==="4"){
        
-        bloco[3].innerHTML ="Caneta Bic"        
+        bloco[3].innerHTML ="Pote de Nutella"        
     }
     else if(elemento==="5"){
       
@@ -51,4 +74,5 @@ function trocaImagem(elemento){
       
         bloco[8].innerHTML ="5 Serenata"        
     }
+    */
 }
