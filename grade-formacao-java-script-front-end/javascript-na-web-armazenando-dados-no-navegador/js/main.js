1f5c95872702c980dc00a2ffccf238c039b088fd
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-12-19 14:30:45 -0300

    update

--- grade-formacao-java-script-front-end/javascript-na-web-armazenando-dados-no-navegador/js/main.js
+++ grade-formacao-java-script-front-end/javascript-na-web-armazenando-dados-no-navegador/js/main.js
@@ -1,6 +1,27 @@
 const form = document.getElementById('novoItem');
 
+
 form.addEventListener("submit",(evento)=>{
-    evento.preventDefault();
-    console.log('funcionou');
+    evento.preventDefault(); //este comando previne que o formulário execute sua função programada original (enviar os dados para algum lugar, que neste caso seria
+    //enviar para própria página e atualizar a mesma já que os dados ainda não tem destino)
+    
+    criaElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome,quantidade){
+    
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML = novoItem.innerHTML + nome;
+
+    const lista = document.querySelector('#lista');
+
+    lista.appendChild(novoItem);
+    
+
+}
