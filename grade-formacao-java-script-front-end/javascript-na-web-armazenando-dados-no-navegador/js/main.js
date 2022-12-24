d3d8b2c51dc5922747e1d36e06ff33f21697b08f
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2022-12-24 09:02:34 -0300

    Update main.js

--- grade-formacao-java-script-front-end/javascript-na-web-armazenando-dados-no-navegador/js/main.js
+++ grade-formacao-java-script-front-end/javascript-na-web-armazenando-dados-no-navegador/js/main.js
@@ -1,46 +1,60 @@
 const form = document.getElementById('novoItem');
-const nome = document.getElementById('nome');
-const itens = [];
+const nom = document.getElementById('nome');
+/*Para que ao recarregar a página o nosso LocalStorage seja consultado e adicione os itens que já tiverem
+sido adicionados iremos modificar o nosso array de objetos abaixo const itens = [], para que se ele identificar itens no LocalStorage ele já crie um array de objetos desses itens ao invés de um array vazio
+
+Importante notificar que como os itens salvos no localStorage são strings, precisamos converter eles de volta
+para que o JavaScript consiga manipula-los, fazemos isso com o comando JSON.parse()*/
+
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //está linha cria uma condicional com ||, em que ou um ou o outro irá ser executado
+
+itens.forEach((elemento) =>{
+criaElemento(elemento)
+}
+    
+)
+
 nome.focus();
 
 form.addEventListener("submit",(evento)=>{
     evento.preventDefault(); //este comando previne que o formulário execute sua função programada original (enviar os dados para algum lugar, que neste caso seria
     //enviar para própria página e atualizar a mesma já que os dados ainda não tem destino)
-    const nom =evento.target.elements['nome']
+    const nome =evento.target.elements['nome']
     const quantidade=evento.target.elements['quantidade']
-
-    criaElemento(nom.value,quantidade.value)
-
-    nom.value = ""; //esvazia o campo de digitação após passar os dados para a função
-    quantidade.value = "";
-    nome.focus();
-})
-
-function criaElemento(nome,quantidade){
-    
-    const novoItem = document.createElement('li');
-    novoItem.classList.add('item')
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML = novoItem.innerHTML + nome;
-
-    const lista = document.querySelector('#lista');
-
-    lista.appendChild(novoItem);
-   /* O modo de armazenamento abaixo só armazena 1 informação por vez, sobrescrevendo as posteriores
+    /* O modo de armazenamento abaixo só armazena 1 informação por vez, sobrescrevendo as posteriores
     localStorage.setItem("nome",nome)
     localStorage.setItem("quantidade",quantidade)
 
     Então iremos usar um array de objetos para guardar múltiplas informações, conforme abaixo:
     */
    const itemAtual = {
-    "nome": nome,
-    "quantidade": quantidade
+    "nome": nome.value,
+    "quantidade": quantidade.value
    }
+   criaElemento(itemAtual)
    itens.push(itemAtual);
-   localStorage.setItem("item",JSON.stringify(itens)) //JSON.stringify transforma o elemento em uma string (o localStorage só consegue ler coisas em string e não em objeto conforme estamos passando)
+   localStorage.setItem("itens",JSON.stringify(itens)) //JSON.stringify transforma o elemento em uma string (o localStorage só consegue ler coisas em string e não em objeto conforme estamos passando)
+
+    
+
+    nome.value = ""; //esvazia o campo de digitação após passar os dados para a função
+    quantidade.value = "";
+    nom.focus();
+})
+
+function criaElemento(item){
+    
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML = novoItem.innerHTML + item.nome;
+
+    const lista = document.querySelector('#lista');
+
+    lista.appendChild(novoItem); 
 
 }
