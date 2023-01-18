7f2425d801153350eb86a145c3785684b3341de4
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-01-18 14:06:30 -0300

    ajuste

--- projetos-pessoais/projeto-site-loja-dois/assets/js/main.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/main.js
@@ -1,16 +1,11 @@
 const departamentos = document.querySelectorAll("[data-departamento]")
 
-for(var i=0;i<departamentos.length;i++)
-{   
-
-}
-
 const produtos = {
     moda: ['Viscose ',
-    'tricoline ','oxford ','two way ','linho ','tecido fantasia ','percal ','malhas ','tecidos de exclusividade'],
-    festa: ['Tule Bordado', 'Cetim Duchese'],
-    cama: ['Colcha','Travesseiro','Protetor'],
-    decor: ['Cortina','Almofada','Tapetes','Tecido p/ Sofa'],
+    'tricoline ','oxford ','two way ','linho ','tecido fantasia ','percal ','malhas ','tecidos de exclusividade','cetim','alfaiatarias','Cambraias'],
+    festa: ['Tule Bordado', 'Cetim Duchese', 'Tule com Pedraria', 'Crepes'],
+    cama: ['Colcha','Travesseiro','Protetor de Colchão', 'Lençol', 'Fronha'],
+    decor: ['Cortina','Almofada','Tapetes','Tecido p/ Sofa','Pingentes','Almofadas'],
     banho:['Toalha','Roupão'],
     mesa:['Sousplat',"Serviço Americano",'Anel p/ Guardanapos']      
     
@@ -21,10 +16,11 @@ elemento.addEventListener("mouseenter",(evento)=>{
     const t = evento.target.dataset.departamento      
     elemento.classList.remove(t)
     elemento.childNodes[1].style.display='none'
+    
     const caixa = document.createElement('div')
     caixa.classList.add('caixa')           
     elemento.appendChild(caixa)  
-    console.log(t)
+    
     
     for(var i=0;i<produtos[t].length;i++){
     const text = document.createElement('p')
@@ -38,17 +34,10 @@ elemento.addEventListener("mouseenter",(evento)=>{
 })
 })
 function removeElemento(item,elemento){     
-        elemento.addEventListener("mouseout",(evento)=>{
+        elemento.addEventListener("mouseleave",(evento)=>{
             elemento.classList.add(evento.target.dataset.departamento)                
             item.remove()
             elemento.childNodes[1].style.display='block'
     })                        
     }  
 
-function modificaElemento(mod){
-    elemento.classList.remove(t)
-    elemento.childNodes[1].style.display='none'
-    const caixa = document.createElement('div')
-    caixa.classList.add('caixa')           
-    elemento.appendChild(caixa) 
-}
