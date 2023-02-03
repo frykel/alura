9e63865274e5b6bcc76bbef0d2357b46656b9eda
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-03 15:01:04 -0300

    update

--- projetos-pessoais/projeto-site-loja-dois/assets/js/menu.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/menu.js
@@ -1,10 +1,30 @@
 const menu = document.querySelector(".produtos")
-const produtosModa=document.querySelectorAll("[data-moda]")
+const produtosSetor=document.querySelectorAll("[data-setor]")
+const setores = ["moda","festa","decor","cama","mesa","banho"]
+const separadores = {
+"moda":[],
+"festa":[],
+"decor":[],
+"cama":[],
+"mesa":[],
+"banho":[]
+}
+const tamanho = Object.keys(separadores).length //calcula o tamanho de um objeto
 
-
-
-console.log(produtosModa[0])
-menu.addEventListener("click",(evento)=>{
-    evento.style.display="block"
-    console.log("eu")
+produtosSetor.forEach((elemento)=>{
+   // console.log(elemento.dataset.setor)
+    //console.log(elemento.innerHTML)
+    console.log(separadores)
+    for(var i=0; i<tamanho;i++){
+            
+        if(elemento.dataset.setor===separadores){
+            separadores[i].push(elemento.innerHTML)
+            console.log(1)
+        }
+    }
+    
 })
+
+
+
+
