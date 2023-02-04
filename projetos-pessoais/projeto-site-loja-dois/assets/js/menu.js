9f22c439f9b5722b601b767134651e8bd0fb9a77
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-03 23:04:57 -0300

    update

--- projetos-pessoais/projeto-site-loja-dois/assets/js/menu.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/menu.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".produtos")
+const menu = document.querySelector("[data-link]")
 const produtosSetor=document.querySelectorAll("[data-setor]")
 const setores = ["moda","festa","decor","cama","mesa","banho"]
 const separadores = {
@@ -14,17 +14,50 @@ const tamanho = Object.keys(separadores).length //calcula o tamanho de um objeto
 produtosSetor.forEach((elemento)=>{
    // console.log(elemento.dataset.setor)
     //console.log(elemento.innerHTML)
-    console.log(separadores)
+    
     for(var i=0; i<tamanho;i++){
             
-        if(elemento.dataset.setor===separadores){
-            separadores[i].push(elemento.innerHTML)
-            console.log(1)
+        if(elemento.dataset.setor===setores[i]){
+            separadores[setores[i]].push(elemento.innerHTML)
+           
         }
     }
+
     
 })
 
+colocaOrdem(separadores)
+
+function colocaOrdem(objeto){
+
+    for(var i=0; i<tamanho;i++){
+        objeto[setores[i]].sort()
+    }
+}
+var cont=0;
+var i=0;
+produtosSetor.forEach((elemento)=>{
+   
+   if(cont>=separadores[setores[i]].length){
+    i++
+    cont=0
+   }
+    elemento.innerHTML=separadores[setores[i]][cont]    
+      cont++  
+      
+    }   
+
+)
+
+menu.addEventListener("click",(elemento)=>{
+if(elemento.target.childNodes[3].style.display==="flex"){
+    elemento.target.childNodes[3].style.display="none"
+}
+else{
+    elemento.target.childNodes[3].style.display="flex"
+}
 
 
+}
+)
 
