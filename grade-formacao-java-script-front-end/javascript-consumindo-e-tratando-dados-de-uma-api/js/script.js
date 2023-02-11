0e1fccb80b56c545b881e75a54aba97b6564fdbc
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-11 12:14:38 -0300

    Update script.js

--- grade-formacao-java-script-front-end/javascript-consumindo-e-tratando-dados-de-uma-api/js/script.js
+++ grade-formacao-java-script-front-end/javascript-consumindo-e-tratando-dados-de-uma-api/js/script.js
@@ -1,4 +1,12 @@
-var consultaCep = fetch('https://viacep.com.br/ws/59066051/json/') //acessa a API viacep para verificar o CEP informado (01001000)
+async function buscaEndereco(){
+var consultaCep = await fetch('https://viacep.com.br/ws/59066050/json/') //acessa a API viacep para verificar o CEP /informado (01001000)
+var consultaCepConvertida = await consultaCep.json() //converte consultaCep para json
+//o parâmetro await só pode ser usado dentro de uma função assincrona (async function)
+    console.log(consultaCepConvertida)
+}
+
+buscaEndereco()
+/*
 .then(resposta => resposta.json()) //como o retorno da API é em bytes, convertemos elas para o formato JSON, que é similar a um objeto em JavaScript
 .then(r =>{ 
     if(r.erro){
@@ -11,5 +19,5 @@ var consultaCep = fetch('https://viacep.com.br/ws/59066051/json/') //acessa a AP
 
 }) //após a linha anterior aplicamos outro método then para imprimir a resposta na tela (no console)
 .catch(erro =>console.log(erro)) //o método catch irá imprimir um erro caso o CEP digitado esteja errado
-
-console.log(consultaCep)
+.finally(mensagem => console.log('Procedimento finalizado!'))
+*/
