c2b4a2f233d5d33abefff50b11677b85ca34a169
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-13 10:28:34 -0300

    ajuste

--- grade-formacao-java-script-front-end/javascript-consumindo-e-tratando-dados-de-uma-api/js/script.js
+++ grade-formacao-java-script-front-end/javascript-consumindo-e-tratando-dados-de-uma-api/js/script.js
@@ -1,11 +1,50 @@
-async function buscaEndereco(){
-var consultaCep = await fetch('https://viacep.com.br/ws/59066050/json/') //acessa a API viacep para verificar o CEP /informado (01001000)
+async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+    try{
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`) //acessa a API viacep para verificar o CEP /informado (01001000)
 var consultaCepConvertida = await consultaCep.json() //converte consultaCep para json
 //o parâmetro await só pode ser usado dentro de uma função assincrona (async function)
+if(consultaCepConvertida.erro){
+    throw Error('CEP não existe')
+}
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+    var bairro = document.getElementById('bairro')
+
+    cidade.value = consultaCepConvertida.localidade
+    logradouro.value = consultaCepConvertida.logradouro
+    estado.value = consultaCepConvertida.uf
+    bairro.value = consultaCepConvertida.bairro
+
     console.log(consultaCepConvertida)
+    return consultaCepConvertida;
+    } catch(erro){
+
+        if(erro.name=="TypeError"){
+            console.log("Digite um CEP válido com 8 dígitos")
+        }
+        else{
+        mensagemErro.innerHTML = `<p>CEP inválido, tenta novamente! </p>`
+        console.log(erro)
+        }
+    }
 }
 
-buscaEndereco()
+var cep = document.getElementById('cep')
+cep.addEventListener("focusout",()=>{
+    buscaEndereco(cep.value)
+
+    
+})
+/*
+let ceps = ['01001000','01001001']
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+
+*/
+
 /*
 .then(resposta => resposta.json()) //como o retorno da API é em bytes, convertemos elas para o formato JSON, que é similar a um objeto em JavaScript
 .then(r =>{ 
