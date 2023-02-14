86faada1b737d3c55a9322374c2d9832385452e3
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-14 09:13:51 -0300

    Update main.js

--- grade-formacao-java-script-front-end/javascript-metodos-de-array/app/main.js
+++ grade-formacao-java-script-front-end/javascript-metodos-de-array/app/main.js
@@ -1,6 +1,6 @@
 let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-
+const elementoParaInserirLivros = document.getElementById('livros')
 getBuscarLivrosDaAPI()
 
 async function getBuscarLivrosDaAPI(){
@@ -8,4 +8,21 @@ async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaAPI)
     livros =  await res.json()
     console.table(livros)
+
+    exibirOsLivrosNaTela(livros)
+}
+function exibirOsLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `<div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$ ${livro.preco}0</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>`
+    })
 }
