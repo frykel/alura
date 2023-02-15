b0f9ee864e94645f97bb0b7289f8b834d3ca1bfb
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-15 15:15:21 -0300

    ajuste

--- grade-formacao-java-script-front-end/javascript-metodos-de-array/app/metodoFilter.js
+++ grade-formacao-java-script-front-end/javascript-metodos-de-array/app/metodoFilter.js
@@ -10,7 +10,26 @@ botoes.forEach(btn =>{
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    const livrosFiltrados = categoria =='disponivel' ? livros.filter(livro => livro.quantidade > 0):livros.filter(livro => livro.categoria == categoria)
+    const livrosFiltrados = categoria =='disponivel' ? filtrarPorDisponibilidade():filtrarPorCategoria(categoria)
    
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == "disponivel"){
+        const valorTotal = calcularOValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valor){
+    
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `<div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valor}</span></p>
+  </div>`
 }
