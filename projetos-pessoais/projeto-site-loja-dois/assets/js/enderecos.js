511b223bb903b67795f0b11db1b6ab219d60c759
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-02-16 15:07:01 -0300

    update

--- projetos-pessoais/projeto-site-loja-dois/assets/js/enderecos.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/enderecos.js
@@ -1,8 +1,22 @@
-const divProdutos = document.querySelector(".produtos")
-const linkProdutos = []
+const divProdutos = document.querySelectorAll(".produtos-setor-item")
+let linkProdutos = []
+const mainLink = 'https://frykel.github.io/alura/projetos-pessoais/projeto-site-loja-dois/assets/json/enderecos.json'
 
+
+buscaListaDeProdutos()
 async function buscaListaDeProdutos(){
-    const r = await fetch('../json/enderecos.json')
-    linkProdutos = r.json()
+    const r = await fetch(mainLink)
+    linkProdutos = await r.json()
+    console.table(linkProdutos["moda"][0].url)
+    exibirLinksNaTela(linkProdutos["moda"])
 }
-console.log(linkProdutos)
+
+function exibirLinksNaTela(links){
+    links.forEach((link) => {
+       
+        divProdutos[0].innerHTML = `
+            <a class="item" data-setor="moda" href="${link.url}">Viscose</a>`
+        
+    });
+
+}
