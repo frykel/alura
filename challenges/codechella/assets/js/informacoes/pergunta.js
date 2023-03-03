461c359b3831e0e15ea0a4ff5bde539c951ae44f
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-03 20:23:27 -0300

    update

--- challenges/codechella/assets/js/informacoes/pergunta.js
+++ challenges/codechella/assets/js/informacoes/pergunta.js
@@ -1,11 +1,24 @@
 
-const botao = document.querySelectorAll("[data-botao]")
+
+const button = document.querySelectorAll("[data-botao]")
 const resposta = document.querySelectorAll("[data-resposta]")
 
-console.log(botao)
 
-botao.forEach(elemento => {
-    elemento.addEventListener("click",((evento)=>{
-        console.log(evento.dataset)
-    }))
+
+button.forEach((elemento) => {
+
+    elemento.addEventListener("click",(evento)=>{
+        console.log(evento.target.dataset.botao)
+        abreCaixa(evento.target.dataset.botao)
+    })
 })
+
+function abreCaixa(botao){
+    if(resposta[botao].style.display=="none"){
+    resposta[botao].style.display="block"
+    }else{
+        resposta[botao].style.display="none"
+    }
+
+}
+    
