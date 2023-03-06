42feb66adf4e6de44981a801ab5384a3a342b347
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-06 09:54:17 -0300

    update

---
+++ challenges/codechella/assets/js/ingresso/dados-ingresso.js
@@ -0,0 +1,15 @@
+
+
+const dados = {
+    "nome" : document.getElementById("nome"),
+    "email" : document.getElementById("email"),
+    "tipo": "cadeira",
+    "data": document.getElementById("data")
+}
+
+const ingresso = {
+    "nome" : document.getElementsByClassName("ingresso__container__corpo__texto__nome")
+
+}
+console.log(dados)
+console.log(ingresso)
