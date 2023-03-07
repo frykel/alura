f18532858beaf60ed82979d7507558e27e39ed16
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-07 15:05:44 -0300

    update

---
+++ challenges/codechella/assets/js/ingresso/dados-api.js
@@ -0,0 +1,25 @@
+
+async function criaPessoa(nome,email,data,ingresso){
+    const conexao = await fetch("http://localhost:3000",{
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+            data: data,
+            ingresso: ingresso
+        })
+    });
+    if(!conexao.ok){
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+
+}
+
+export const conectaApi = {
+    criaPessoa
+}
--- challenges/codechella/assets/js/ingresso/imprime-ingresso.js
+++ challenges/codechella/assets/js/ingresso/imprime-ingresso.js
@@ -1,6 +1,17 @@
-export default function imprimeIngresso(nome,tipo){
-const localNome = document.querySelector(".ingresso__bonus")
-const localSetor = document.querySelector(".ingresso__setor")
-localNome.innerHTML = nome
-localSetor.innerHTML = tipo
+import { conectaApi } from "./dados-api.js";
+
+const formulario = document.querySelector("#form")
+
+async function criaPessoa(evento){
+    evento.preventDefault();
+    const nome = document.querySelector("#nome").value; //queremos só o que o usuário digitar, então .value
+    const email = document.querySelector("#email").value;
+    const data = document.querySelector("#data").value;
+    const ingresso = document.querySelector("#ingresso").value;
+    
+    await conectaApi.criaPessoa(nome, email, data, ingresso)
+
+    window.location.href = "../pages/pos-compra-ingresso.html";
 }
+
+formulario.addEventListener("submit", evento => criaPessoa(evento))
