2b923acb34be9794285871dff36f501bf3ffa3ab
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-09 08:38:59 -0300

    update

--- challenges/codechella/assets/js/trocar-tema.js
+++ challenges/codechella/assets/js/trocar-tema.js
@@ -3,8 +3,11 @@ const botao = document.querySelector("[data-tema]")
 const cabecalho = document.querySelector(".cabecalho")
 const bannerPrincipal = document.querySelector("#banner__principal")
 const corpo = document.querySelector(".corpo")
-console.log(botao)
+const imagemData = document.querySelector(".data__imagem__arquivo")
+const tituloBandas = document.querySelector(".atracoes__titulo")
+const tituloDia = document.querySelectorAll(".lineup__dia__titulo")
 let cont = 0
+
 botao.addEventListener("click",(evento)=>{
     
     if(cont==0){
@@ -15,6 +18,13 @@ botao.addEventListener("click",(evento)=>{
         bannerPrincipal.classList.add('banner__boreal','banner__texto__boreal')
 
         corpo.classList.add('corpo__boreal')
+
+        imagemData.src="assets/imagens/body/desktop/sobre-boreal.jpg"
+
+        tituloDia.forEach((evento)=>{
+            evento.classList.add("lineup__dia__titulo__boreal") 
+        })
+         
         
         
     
@@ -26,7 +36,14 @@ botao.addEventListener("click",(evento)=>{
         cabecalho.classList.remove('cabecalho__container__boreal')
 
         bannerPrincipal.classList.remove('banner__boreal','banner__texto__boreal')
+
         corpo.classList.remove('corpo__boreal')
+
+        imagemData.src="assets/imagens/body/desktop/sobre-pessoas.jpg"
+
+        tituloDia.forEach((evento)=>{
+            evento.classList.remove("lineup__dia__titulo__boreal") 
+        })  
     cont=0
     }
 
