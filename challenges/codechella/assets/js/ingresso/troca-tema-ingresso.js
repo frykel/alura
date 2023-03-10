d9f75bdced8e0495bb7816a0114d45435e9558cf
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-10 13:07:44 -0300

    update

--- challenges/codechella/assets/js/ingresso/troca-tema-ingresso.js
+++ challenges/codechella/assets/js/ingresso/troca-tema-ingresso.js
@@ -1,18 +1,18 @@
 const submitBotao = document.querySelector(".botao__compra__ingresso")
-
+console.log(submitBotao)
 let cont = 0
 botao.addEventListener("click",(elemento)=>{
     if(cont==0){
-    submitBotao.forEach((evento)=>{
     
-        evento.classList.add("botao__compra__ingresso__boreal")
-    })
+    
+        submitBotao.classList.add("botao__compra__ingresso__boreal")
+    
     cont++
 }else{
-    submitBotao.forEach((evento)=>{
+   
 
-        evento.classList.remove("botao__compra__ingresso__boreal")
-    })
+        submitBotao.classList.remove("botao__compra__ingresso__boreal")
+
     cont=0
 
 }
