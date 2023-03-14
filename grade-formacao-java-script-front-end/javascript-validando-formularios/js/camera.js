54a71e158f85d11a63661b39255b6b064c7455c9
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-14 14:38:11 -0300

    Update camera.js

--- grade-formacao-java-script-front-end/javascript-validando-formularios/js/camera.js
+++ grade-formacao-java-script-front-end/javascript-validando-formularios/js/camera.js
@@ -5,6 +5,7 @@ const video = document.querySelector("[data-video]")
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]")
 const canvas = document.querySelector("[data-video-canvas]")
 const mensagem = document.querySelector("[data-mensagem]")
+const botaoDeEnviarFoto = document.querySelector("[data-enviar]")
 
 let imagemURL = ""
 
@@ -26,4 +27,15 @@ botaoTirarFoto.addEventListener("click", function(){
     campoCamera.style.display = "none"
 
     mensagem.style.display = "block"
+})
+
+botaoDeEnviarFoto.addEventListener("click", () =>{
+    const receberDadosExistentes = localStorage.getItem("cadastro")
+    const converteRetorno = JSON.parse(receberDadosExistentes)
+
+    converteRetorno.imagem = imagemURL
+
+    localStorage.setItem("cadastro", JSON.stringify(converteRetorno))
+
+    window.location.href = "./abrir-conta-form-3.html"
 })
