f72ce66c964f6aa33d220a3f6037cbcc3ef8538c
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-14 14:25:09 -0300

    Update camera.js

--- grade-formacao-java-script-front-end/javascript-validando-formularios/js/camera.js
+++ grade-formacao-java-script-front-end/javascript-validando-formularios/js/camera.js
@@ -2,12 +2,28 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]")
 const campoCamera = document.querySelector("[data-camera]")
 const video = document.querySelector("[data-video]")
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]")
+const canvas = document.querySelector("[data-video-canvas]")
+const mensagem = document.querySelector("[data-mensagem]")
+
+let imagemURL = ""
 
 botaoIniciarCamera.addEventListener("click", async function () {
-    const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-
+    const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false}); //esse código acessa o dispositivo de câmera do usuário
+    /*Foi necessário usar função assíncrona porque é necessário esperar a câmera do usuário ser ligada bem como
+    possíveis permissões que o navegador possa solicitar (como permitir vídeo)*/
     botaoIniciarCamera.style.display = "none";
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+})
+
+botaoTirarFoto.addEventListener("click", function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    imagemURL = canvas.toDataURL("imagem/jpg")
+
+    campoCamera.style.display = "none"
+
+    mensagem.style.display = "block"
 })
