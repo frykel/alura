8c099c1d6dbe5927dc68dc47ccd977d5a1c79e6c
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-15 15:05:33 -0300

    update

---
+++ grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/App.js
@@ -0,0 +1,13 @@
+
+import Banner from './componentes/banner/Banner.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/componentes/banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner Principal da páginado Organo"/>
+        </header>
+    )
+
+}
+
+export default Banner
