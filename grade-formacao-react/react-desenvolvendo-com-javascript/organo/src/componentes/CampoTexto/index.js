791a54e962951015f77db350980038b1774de83d
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-20 15:12:17 -0300

    update

--- grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
+++ grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
@@ -2,12 +2,16 @@ import './CampoTexto.css'
 const CampoTexto = (props) =>{
      /*O props é o nome "padrão" utilizado por Devs para o parâmetro do componente. Significa propriedades.
      Mas claro que qualquer outro nome poderia ser utilizado*/ 
-
+     let valor = ""
      const placeholderModificada = `${props.placeholder}...`
+     const aoDigitado = (evento) =>{
+      valor = evento.target.value
+         console.log(valor)
+     }
     return(
        <div className="campo-texto">
        <label>{props.label}</label>
-       <input placeholder={placeholderModificada}></input>
+       <input onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}></input>
        </div>
     )
 }
--- grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/componentes/Formulario/index.js
+++ grade-formacao-react/react-desenvolvendo-com-javascript/organo/src/componentes/Formulario/index.js
@@ -4,17 +4,25 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
-    const times = ["Programação","Front-End","Data Science","Devops","UX e Design","Mobile","Inovação e Gestão"]
+    const times = ["","Programação","Front-End","Data Science","Devops","UX e Design","Mobile","Inovação e Gestão"]
+const aoSalvar = (evento) =>{
+    evento.preventDefault()
+    
+    console.log("Salvado com sucesso")
+    
+}
     return (
 
         <section className='formulario-container'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <CampoTexto obrigatorio = "true" label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto obrigatorio = "true" label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio = "true" label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label = "Times:" itens ={times}/>
-                <Botao texto = "Criar card"/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )   
