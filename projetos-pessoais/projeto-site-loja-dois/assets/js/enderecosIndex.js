c6f1f8d9781bdde51868aecbce69a40eb0952066
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-03-30 15:21:10 -0300

    update

--- projetos-pessoais/projeto-site-loja-dois/assets/js/enderecosIndex.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/enderecosIndex.js
@@ -1,33 +1,44 @@
 import { conexaoApi } from "./api.js";
-const produtos = document.querySelectorAll("[data-setor]")
+const produtosDepartamento = document.querySelectorAll("[data-setor]")
 
 for(var i=0;i<6;i++){
-    if(produtos[i].dataset.setor==produtos[i].dataset.setor){
-        console.log(produtos[i].dataset.setor)
-        exibirLinksNaTela(produtos[i].dataset.setor,i)
+    if(produtosDepartamento[i].dataset.setor==produtosDepartamento[i].dataset.setor){
+       
+        exibirLinksNaTela(produtosDepartamento[i].dataset.setor,i)
        
     }
 }
-async function exibirLinksNaTela(departamento,numeroDepartamento){
+async function exibirLinksNaTela(departamento,num){
     
     const linksApi = await conexaoApi.buscaListaDeProdutos(departamento)
-    const tituloDoSetor = document.createElement("h3")
+    console.table(linksApi[linksApi.length-1])
+
+    let tituloDoSetor = document.createElement("h3")
     tituloDoSetor.className="produtos-setor-titulo"
     tituloDoSetor.innerHTML=departamento
     
+    produtosDepartamento[num].appendChild(tituloDoSetor)
+
+    linksApi.forEach(links => produtosDepartamento[num].appendChild(criaLink(links.titulo,links.urlIndex)));
+
+}
+function criaLink(nome,link){
+    const categoria = document.createElement("div")
+    categoria.innerHTML =`
+        <a href =${link}>${nome}</a>
+    `
+    return categoria
+}
+    /*const tituloDoSetor = document.createElement("h3")
+    tituloDoSetor.className="produtos-setor-titulo"
+    tituloDoSetor.innerHTML=departamento
+      
     produtos[numeroDepartamento].appendChild(tituloDoSetor)
 
     const blocoDeLink = document.createElement("div")
     blocoDeLink.className = "produtos-setor-item"
     produtos[numeroDepartamento].appendChild(blocoDeLink)
 
-    linksApi.forEach(link => produtos[numeroDepartamento].appendChild(constroiLinks(link.urlindex,blocoDeLink,link.titulo)));
+   //linksApi.forEach(link => produtos[numeroDepartamento].appendChild(constroiLinks(link.urlindex,blocoDeLink,link.titulo)));
+*/
 
-}
-function constroiLinks(urlIndex,bloco,titulo){
-
-    bloco.innerHTML=`<a class="item" href="${urlIndex}">${titulo}</a>`
-    
-    
-    
-}
