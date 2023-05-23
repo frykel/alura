973806b9f6409fdc4cf4584eeb82e7e99420372b
Silvio Fernandes <98838398+frykel@users.noreply.github.com>
2023-05-23 14:33:35 -0300

    ajuste

--- projetos-pessoais/projeto-site-loja-dois/assets/js/principal/banner-rotativo.js
+++ projetos-pessoais/projeto-site-loja-dois/assets/js/principal/banner-rotativo.js
@@ -1,7 +1,15 @@
 var currentImageIndex = 0;
         var images = document.querySelectorAll('.banner img');
-        var prevArrow = document.querySelector('.arrow.prev');
-        var nextArrow = document.querySelector('.arrow.next');
+        var prevArrow = document.querySelector('.prev');
+        var nextArrow = document.querySelector('.next');
+        var indicatorsContainer = document.querySelector('.indicators');
+
+        function setActiveIndicator(index) {
+            var indicators = document.querySelectorAll('.indicator');
+            for (var i = 0; i < indicators.length; i++) {
+                indicators[i].classList.toggle('active', i === index);
+            }
+        }
 
         function showImage(index) {
             for (var i = 0; i < images.length; i++) {
@@ -15,6 +23,7 @@ var currentImageIndex = 0;
                 currentImageIndex = 0;
             }
             showImage(currentImageIndex);
+            setActiveIndicator(currentImageIndex);
         }
 
         function prevImage() {
@@ -23,9 +32,26 @@ var currentImageIndex = 0;
                 currentImageIndex = images.length - 1;
             }
             showImage(currentImageIndex);
+            setActiveIndicator(currentImageIndex);
         }
 
         nextArrow.addEventListener('click', nextImage);
         prevArrow.addEventListener('click', prevImage);
 
-        setInterval(nextImage, 5000);
+        for (var i = 0; i < images.length; i++) {
+            var indicator = document.createElement('div');
+            indicator.classList.add('indicator');
+            indicator.onclick = function(index) {
+                return function() {
+                    currentImageIndex = index;
+                    showImage(currentImageIndex);
+                    setActiveIndicator(currentImageIndex);
+                };
+            }(i);
+            indicatorsContainer.appendChild(indicator);
+        }
+        showImage(currentImageIndex);
+        setActiveIndicator(currentImageIndex);
+
+        
+        setInterval(nextImage, 6000);
